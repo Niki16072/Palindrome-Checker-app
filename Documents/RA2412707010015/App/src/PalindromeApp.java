4e05fa53d1cc4bb088831f9251c923c625de806d
parnavvii <parnavisharma311@gmail.com>
2026-02-27 14:55:09 +0530

    Welcome modified Message

--- Documents/RA2412707010015/App/src/PalindromeApp.java
+++ Documents/RA2412707010015/App/src/PalindromeApp.java
@@ -2,7 +2,7 @@ public class PalindromeApp {
     static void main() {
         System.out.println("Welcome to Palindrome Management System");
         System.out.println("Version : 1.0");
-        System.out.println("System intialized successfully.");
+        System.out.println("System intialized successfully  done.");
 
     }
 }
