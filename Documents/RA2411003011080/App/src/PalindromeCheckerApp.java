877f884621609f3384b38484e4067fe2c8f39014
Niki16072 <nikiapsb@gmail.com>
2026-03-09 15:57:29 +0530

    Delete Documents directory

--- Documents/RA2411003011080/App/src/PalindromeCheckerApp.java
+++
@@ -1,9 +0,0 @@
-import javax.rmi.ssl.SslRMIClientSocketFactory;
-
-public class PalindromeCheckerApp {
-    static void main() {
-        System.out.println("Welcome to PalindromeCheckerApp");
-        System.out.println("Version 1.0");
-        System.out.println("System Initialised Successfully");
-    }
-}
